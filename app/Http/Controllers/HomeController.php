49d6da36c13ae8afc4bd3b44d7067df646605861
tawhan695 <tanongsak.bo@ku.th>
2020-10-16 20:28:29 +0700

    100

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,13 +27,114 @@ class HomeController extends Controller
     public function index()
     {   
         
-        $data = array();
+       
         $league = DB::select('SELECT league FROM predict INNER JOIN link_match ON link_match.url= predict.url GROUP BY league');
+        $date = DB::select('SELECT predict.date FROM predict INNER JOIN link_match ON link_match.url= predict.url GROUP BY predict.date');
+        $accuracy = DB::select('SELECT * FROM accuracy ORDER BY accuracy.date DESC LIMIT 1');
+        $accuracy2 = DB::select('SELECT * FROM accuracy ORDER BY accuracy.date DESC LIMIT 20');
+        $predict = DB::select('SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where predict.predict = link_match.FTR ');
+        $all = DB::select('SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where  link_match.FTR !=""');
+       
+        $percent = 0;
+        if (count($all) != 0){
+
+            $percent =  (count($predict) / count($all)) *100;
+        }else{
+            $percent = 0;
+        }
+        // รวมdate
+        $date_arr =  array();
+        for ($i=0; $i <count($date) ; $i++) { 
+            if($i == 0){
+                array_push($date_arr,$date[$i]->date);
+            }
+            for ($j=0; $j < count($date_arr) ; $j++) { 
+                if($date[$i]->date != $date_arr[$j]){
+                    array_push($date_arr,$date[$i]->date);
+                }
+            }
+        }
+        $last_date = count($date_arr)-1;
+        $data = array();
         foreach ($league as $key => $value) {
-            array_push($data,DB::select('SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where league = "'.$value->league.'" ORDER BY link_match.time DESC'));
+            $sql = 'SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where link_match.league = "'.$value->league.'" and predict.date ="'.$date_arr[$last_date].'" ORDER BY link_match.time DESC';
+        //    echo $sql;
+            $le =  DB::select($sql);
+            // var_dump($le);
+            array_push($data,$le);
             
         }
-        // var_dump($data);
-        return view('home')->with(['Tablepredict'=> $data]);
+        $list_date = array();
+        for ($i=count($date_arr); $i > 0 ; $i--) { 
+            // echo $i;
+            if ((count($date_arr)-6 == $i )) {
+            break;
+            }
+            array_push( $list_date,$date_arr[$i-1]);
+            // echo $i;
+            // echo $date_arr[$i-1];
+        }
+        // foreach ($date as $key => $dd) {
+           
+        // }
+        // var_dump($list_date);
+        session()->push('active',$date_arr[$last_date]);
+        return view('home')->with(['Tablepredict'=> $data,'accuracy'=>$accuracy,'percent'=>$percent,'history'=> $accuracy2,'datelist' =>$list_date]);
     }
+    public function date(Request $request){
+        
+        $league = DB::select('SELECT league FROM predict INNER JOIN link_match ON link_match.url= predict.url GROUP BY league');
+        $date = DB::select('SELECT predict.date FROM predict INNER JOIN link_match ON link_match.url= predict.url GROUP BY predict.date');
+        $accuracy = DB::select('SELECT * FROM accuracy ORDER BY accuracy.date DESC LIMIT 1');
+        $accuracy2 = DB::select('SELECT * FROM accuracy ORDER BY accuracy.date DESC LIMIT 20');
+        $predict = DB::select('SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where predict.predict = link_match.FTR ');
+        $all = DB::select('SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where  link_match.FTR !=""');
+        
+        $percent = 0;
+        if (count($all) != 0){
+            
+            $percent =  (count($predict) / count($all)) *100;
+        }else{
+            $percent = 0;
+        }
+        // รวมdate
+        $date_arr =  array();
+        for ($i=0; $i <count($date) ; $i++) { 
+            if($i == 0){
+                array_push($date_arr,$date[$i]->date);
+            }
+            for ($j=0; $j < count($date_arr) ; $j++) { 
+                if($date[$i]->date != $date_arr[$j]){
+                    array_push($date_arr,$date[$i]->date);
+                }
+            }
+        }
+        $last_date = $request->date;
+        $data = array();
+        foreach ($league as $key => $value) {
+            $sql = 'SELECT * FROM predict INNER JOIN link_match ON link_match.url=predict.url where link_match.league = "'.$value->league.'" and predict.date ="'.$last_date.'" ORDER BY link_match.time DESC';
+            //    echo $sql;
+            $le =  DB::select($sql);
+            // var_dump($le);
+            array_push($data,$le);
+            
+        }
+        $list_date = array();
+        for ($i=count($date_arr); $i > 0 ; $i--) { 
+            // echo $i;
+            if ((count($date_arr)-6 == $i )) {
+            break;
+        }
+        array_push( $list_date,$date_arr[$i-1]);
+        // echo $i;
+        // echo $date_arr[$i-1];
+    }
+    // foreach ($date as $key => $dd) {
+        
+        // }
+        // var_dump($list_date);
+        session()->push('active',$request->date);
+        return view('home')->with(['Tablepredict'=> $data,'accuracy'=>$accuracy,'percent'=>$percent,'history'=> $accuracy2,'datelist' =>$list_date]);
+    }
+    
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,10 @@
 
 <style>
     .f-100{
-        font-size: 5px
+        font-size: 5px;
+    }
+    #f-100{
+        font-size: 5px;
     }
 
 </style>
@@ -13,13 +16,86 @@
     responsive: true
 } );
 </script>
+{{-- Modal --}}
+ <!-- Modal -->
+ <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title text-danger">20 ล่าสุด</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+            @foreach ($history as $item)
+                <div class="p-1 m-1 text-center btn-warning">datetime :{{$item->date}} Accuracy: {{$item->Accuracy}}%</div>
+            @endforeach
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+</div>
+{{-- Modal --}}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 p-1">
+            <div class="card-header bg-danger text-white">        
+            </div>
+            <div class="card ">
+                <div class="card-body f-100 ">
+                    <div class="alert alert-success">
+                        <strong>update model !</strong> date @foreach ( $accuracy as $item)
+                        {{$item ->date}} accuracy {{$item->Accuracy}}% <br>
+                        ระบบจะสร้างโมเดลใหม่ 1 ครั้งต่อสัปดาห์ <a class=" text-info " data-toggle="modal" data-target="#myModal" >ดูเพิ่มเติม ประวัติความแม่นยำ</a>
+                        @endforeach
+                    </div>
+                    <div class="alert alert-success">
+                        <strong>Accuracy in predict  </strong>  @foreach ( $accuracy as $item)
+                         accuracy {{$percent}}%
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 p-1 m-1">
+            {{-- <div class="card-header"> --}}
+                <div class="row">
+
+                    @foreach ($datelist as $item)
+                        <div class="col-4 col-md-4 col-lg-2">
+                            @if (session('active')[0] == $item)
+                                
+                                <a class="btn btn-success " href="{{route('date',['date'=>$item])}}">{{$item}}</a>
+                                @else
+                                
+                                <a class="btn btn-info " href="{{route('date',['date'=>$item])}}">{{$item}}</a>
+                                {{-- <a class="btn btn-info " href="date/?date={{$item}}">{{$item}}</a> --}}
+                            @endif
+                        </div>
+                    @endforeach
+                   
+                  
+                {{-- </div> --}}
+            </div>
+        </div>
+    </div>
+</div>
+
 @foreach ($Tablepredict as $item)
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 p-1">
-                <div class="card">
+                <div class="card ">
                    
-                    <div class="card-header"> @php
+                    <div class="card-header bg-danger text-white"> @php
                         foreach ($item as $key => $value) {
                             echo $value->league;
                             break;
@@ -27,22 +103,28 @@
                     @endphp
                     </div>
 
-                    <div class="card-body ">
-                        <table class="table" id="myTable">
+                    <div class="card-body f-100 ">
+                        <table class="table table-borderless" id="myTable">
                             <thead>
                                 <tr>
-                                    <th class="f-100" scope="col">Time</th>
-                                    <th class="f-100" scope="col">Home</th>
-                                    <th class="f-100" scope="col">FTR</th>
-                                    <th class="f-100" scope="col">Away</th>
-                                    <th class="f-100" scope="col">Predict</th>
+                                    <th class="f-100 text-info" scope="col">Time</th>
+                                    <th class="f-100 text-info" scope="col">Home</th>
+                                    <th class="f-100 text-info" scope="col">FTR</th>
+                                    <th class="f-100 text-info" scope="col">Away</th>
+                                    <th class="f-100 text-info" scope="col">Predict</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($item as  $value)
-                                    <tr>
-                                        <th class="f-100">{{$value->time}}</th>
-                                        <th class="f-100 overflow-auto">{{$value->home}}</th>
+                                    <tr class="@if ($value->FTR  == $value->predict) bg-success @endif">
+                                        <th class="f-100"><p>{{Str::beforeLast($value->time, 'T')}} <br> {{Str::of($value->time)->after('T')}}</p></th>
+                                        {{-- <th class="f-100">{{Str::beforeLast($value->time, 'T')}}</th> --}}
+                                        <th class="f-100 overflow-auto" data-toggle="tooltip" title="{{$value->home}}" ><a href="">{{Str::limit($value->home, 12, ' ...')}}</a></th>
+                                        <script>
+                                            $(document).ready(function(){
+                                              $('[data-toggle="tooltip"]').tooltip();   
+                                            });
+                                            </script>
                                         <th class="f-100">
                                             @if (!$value->FTR)
                                                 --
@@ -51,8 +133,13 @@
                                                 
                                             @endif
                                         </th>
-                                        <th class="f-100 overflow-auto">{{$value->away}}</th>
-                                        <th class="f-100">{{$value->predict}}</th>
+                                        {{-- <th class="f-100 overflow-auto">{{Str::limit($value->away, 18, ' ...')}}</th> --}}
+                                        <th class="f-100 overflow-auto" data-toggle="tooltip" title="{{$value->away}}" ><a href="">{{Str::limit($value->away, 12, ' ...')}}</a></th>
+                                        <th class="f-100">{{$value->predict}} <br> Win rate 
+                                            <br> H:{{$value->H}}%
+                                            <br> D:{{$value->D}}%
+                                            <br> A:{{$value->A}}%
+                                        </th>
         
                                     </tr>
                                 @endforeach
